c6277e39b0ad899c742788e42b0559944aa74091
hoanganhvu271 <hoanganhvu271103@gmail.com>
2024-04-24 22:44:11 +0700

    x

--- app3.js
+++ app3.js
@@ -1,63 +1,56 @@
 
 
-const { MongoClient, ObjectId } = require('mongodb');
+const { MongoClient } = require('mongodb');
 
-// Code cho kịch bản trên.
 async function main() {
-    // Connect
 
-    // Đây phải là 1 replica set.
     const uri = 'mongodb://localhost:27018';
     client = await MongoClient.connect(uri);
 
     const db = client.db("demo");
 
     try {
-        // Chuyển tiền từ A sang C nhưng C không tồn tại trong cơ sở dữ liệu.
-        await transfer('A', 'C', 50000);
-
+        await transfer('A', 'B', 50000);
     } catch (error) {
-        console.error(error)
-        console.log(error.message)
-        console.log('\n<>. Giao dịch không thành công\n')
+        console.log('\nGiao dịch không thành công\n')
     }
 
     async function transfer(from, to, amount) {
         const session = client.startSession();
         session.startTransaction();
+
         try {
-            // Tìm kiếm tài khoản 'from'.
+
+            // 1. 
             const opts = { session, returnOriginal: true };
-            var objectId = new ObjectId("6627d56533f36f355d215bd5");
             const filterFrom = { name: from };
             const resultFrom = await db.collection('tmp').findOne(filterFrom);
             if (!resultFrom) {
                 throw new Error(`Không tìm thấy tài khoản có tên ${from}`);
             }
+
+            //2. 
             const A = resultFrom;
             const newBalanceA = A.balance - amount;
-
-            console.log("<>. Kiểm tra tài khoản có đủ tiền hay không?")
             if (newBalanceA < 0) {
                 throw new Error(`Không đủ tiền trong tài khoản ${from}`);
             }
 
-            console.log("<>. Trừ tiền của " + from)
+            // 3. 
             await db.collection('tmp').updateMany(filterFrom, { $set: { balance: newBalanceA } }, opts);
 
-            console.log("<>. Tìm kiếm tài khoản" + to)
-            objectId = new ObjectId("6627d56533f36f355d215bd5");
+
+            // 4.
             const filterTo = { name: to };
             const resultTo = await db.collection('tmp').findOne(filterTo);
             if (!resultTo) {
                 throw new Error(`Không tìm thấy tài khoản có tên ${to}`);
             }
 
-            console.log("<>. Cộng tiền cho tài khoản " + to)
+            // 5. 
             const B = resultTo;
             const newBalanceB = B.balance + amount;
             await db.collection('tmp').updateMany(filterTo, { $set: { balance: newBalanceB } }, opts);
-
 
             // Commit Transaction
             await session.commitTransaction();
@@ -65,7 +58,6 @@ async function main() {
             return { from: A, to: B };
 
         } catch (error) {
-
             // Abort Transaction
             await session.abortTransaction();
             session.endSession();
@@ -73,9 +65,6 @@ async function main() {
         }
     }
 }
-
-
-// Chuyển tiền
 
 
 main(); 
